d55fd93c013157eb7520b839cbafc4c735d23ace
SimonTust <simontust1986@outlook.es>
2023-12-08 22:13:39 -0300

    test9

--- dproject/urls.py
+++ dproject/urls.py
@@ -20,6 +20,7 @@ from tasks import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home, name='home'),
     path('home/',views.home, name='home'),
     path('sobremi/',views.sobremi, name='sobremi'),
     path('signup/',views.signup, name='signup'),
